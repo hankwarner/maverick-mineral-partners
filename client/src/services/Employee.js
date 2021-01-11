4939e3aa20b340f210faf30c6fa15da12dcd9bbf
hankwarner <warnerh1576@gmail.com>
2021-01-11 18:00:08 -0500

    Update api routing.

--- client/src/services/Employee.js
+++ client/src/services/Employee.js
@@ -2,7 +2,7 @@ import Api from './Api';
 
 export default {
     async getEmployees() {
-        var path = 'employees';
+        var path = 'api/employees';
         var response = await Api().get(path);
         
         return response;
--- client/src/services/Testimonial.js
+++ client/src/services/Testimonial.js
@@ -2,7 +2,7 @@ import Api from './Api';
 
 export default {
     async getTestimonials() {
-        var path = 'testimonial';
+        var path = 'api/testimonial';
         var response = await Api().get(path);
         
         return response;
